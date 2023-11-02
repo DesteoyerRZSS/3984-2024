2057b7ef1b5ce472a069613c5eed142025b7e20d
ChoccyMalk <79180114+choccymalk@users.noreply.github.com>
2023-11-02 13:18:30 -0400

    Update RobotContainer.java

--- testingPID2/src/main/java/frc/robot/RobotContainer.java
+++ testingPID2/src/main/java/frc/robot/RobotContainer.java
@@ -59,7 +59,7 @@ public class RobotContainer {
   new JoystickButton(driver, XboxController.Button.kRightBumper.value);
   private final JoystickButton light = 
     new JoystickButton(driver, XboxController.Button.kB.value);
-  private final JoystickButton absolute = g
+  private final JoystickButton absolute = 
     new JoystickButton(driver, XboxController.Button.kA.value);
   private final JoystickButton halfSpeed = 
     new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
